8fccc4c4fe5fa9a6cd2a56d1f1bd8a9541e71a4f
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-08-11 16:52:58 +0100

    added socket.io to client

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import useLocalStorage from "./hooks/useLocalStorage";
 import Dashboard from "./components/Dashboard";
 import { ContactsProvider } from "./contexts/ContactsProvider";
 import { ConversationsProvider } from "./contexts/ConversationsProvider";
+import { SocketProvider } from "./contexts/SocketProvider";
 
 function App() {
   const [id, setId] = useLocalStorage("id")
 
   const dashboard = (
-    <ContactsProvider>
-      <ConversationsProvider id={id}>
-        <Dashboard id={id} />
-      </ConversationsProvider>
-    </ContactsProvider>
+    <SocketProvider id={id}>
+      <ContactsProvider>
+        <ConversationsProvider id={id}>
+          <Dashboard id={id} />
+        </ConversationsProvider>
+      </ContactsProvider>
+    </SocketProvider>
   )
 
   return (
