37572e651eff938c38c7aabd497d78701813febb
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-08-11 11:24:42 +0100

    added sidebar component with basic Nav bar

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from "react"
 import './App.css';
 import Login from "./components/Login"
 import useLocalStorage from "./hooks/useLocalStorage";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   const [id, setId] = useLocalStorage("id")
 
   return (
-    <>
-      {id}
-      <Login onIdSubmit={setId} />
-    </>
+    id ? <Dashboard id={id}/> : <Login onIdSubmit={setId} />
   );
 }
 
