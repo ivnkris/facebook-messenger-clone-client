6839764513bd7341c303a592f8160f6bfb84d79e
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-08-11 13:54:10 +0100

    added conversations to sidebar

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import Login from "./components/Login"
 import useLocalStorage from "./hooks/useLocalStorage";
 import Dashboard from "./components/Dashboard";
 import { ContactsProvider } from "./contexts/ContactsProvider";
+import { ConversationsProvider } from "./contexts/ConversationsProvider";
 
 function App() {
   const [id, setId] = useLocalStorage("id")
 
   const dashboard = (
     <ContactsProvider>
+      <ConversationsProvider>
       <Dashboard id={id} />
+      </ConversationsProvider>
     </ContactsProvider>
   )
 
